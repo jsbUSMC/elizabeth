c0060ce4b1392e0c45159837d6da09e21ed6b882
John Bissonette <jsb.work.universal@gmail.com>
2016-07-20 19:50:50 +0000

    added info to index.html and made style and script changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var util = require("gulp-util");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var cleanCSS = require("gulp-clean-css");
+var uglify = require("gulp-uglify");
 var gzip = require("gulp-gzip");
 var concat = require("gulp-concat");
 var sourcemaps = require("gulp-sourcemaps");
@@ -17,7 +18,7 @@ gulp.task("styles", function() {
   gulp.src("src/scss/**/*.scss")
     .pipe(sourcemaps.init())
     .pipe(sass({
-      includePaths: bourbon,
+      includePaths: bourbon, 
       includePaths: neat
     }))
     .pipe(autoprefixer())
@@ -25,5 +26,23 @@ gulp.task("styles", function() {
     .pipe(rename("all.min.css"))
     .pipe(cleanCSS())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest("dist/css"))
+    .pipe(gulp.dest("dist/css"));
 });
+
+// compile js
+gulp.task("scripts", function() {
+  gulp.src("src/js/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(concat("all.js"))
+    .pipe(rename("all.min.js"))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task("watch", function() {
+    gulp.watch("src/scss/**/*.scss", ["styles"]);
+    gulp.watch("src/js/**/*.js", ["scripts"]);
+});
+
+gulp.task("default", ["watch"]);
---
+++ src/js/page.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#specialbutton").click(function() {
+        $("p").after("<h3>This fuckin shit works</h3>");
+    });
+});
